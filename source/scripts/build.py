9e15d4bd54a274fb428c740afa9632bac1aac551
dildus1337 <Aiwiti333@gmail.com>
2020-06-27 18:34:42 +0300

    Дальнейшая настройка сервера.

--- source/scripts/build.py
+++ source/scripts/build.py
@@ -22,15 +22,17 @@ file_server_cfg.close()
 # TODO: Нормальный цикл FOR.
 i = 0
 while (i < len(data_server_cfg)):
-    if (data_server_cfg[i] == "rcon_password $RCON_PASSWORD"):
+    if (data_server_cfg[i] == "rcon_password $RCON_PASSWORD" + '\n'):
         # TODO: Случайная генерация RCON-пароля.
-        data_server_cfg[i] = "rcon_password (x1PLACE!HOLDER1x)"
+        data_server_cfg[i] = "rcon_password (x1PLACE!HOLDER1x)" + '\n'
+    i += 1
 
 path_new_server_cfg = path_output_folder + "/server.cfg"
 file_new_server_cfg = open(path_new_server_cfg, "w")
 i = 0
 while (i < len(data_server_cfg)):
-    file_new_server_cfg.write(data_server_cfg[i] + '\n')
+    file_new_server_cfg.write(data_server_cfg[i])
+    i += 1
 file_new_server_cfg.close()
 
 
@@ -38,13 +40,20 @@ file_new_server_cfg.close()
 # # # # #
 # Перенос требуемых исполняемых файлов и лицензии SAMP.
 # # # # #
+server_os = "windows"
+
 announce = False
 npc = False
 
-path_server = "../exec/samp03svr"
 path_samp_license = "../lic/SAMP_LICENSE.txt"
-path_announce = "../exec/announce"
-path_npc = "../exec/samp-npc"
+if (server_os == "windows"):
+    path_server = "../exec/windows/samp-server.exe"
+    path_announce = "../exec/windows/announce.exe"
+    path_npc = "../exec/windows/samp-npc.exe"
+elif (server_os == "linux"):
+    path_server = "../exec/linux/samp03svr"
+    path_announce = "../exec/linux/announce"
+    path_npc = "../exec/linux/samp-npc"
 
 shutil.copy2(path_server, path_output_folder)
 shutil.copy2(path_samp_license, path_output_folder)
